334c73116648549e7ac0961c1edc37865c63c9be
Junior <jhurtado@plannerapp.net>
2024-10-18 18:27:27 -0600

    agregando las sucursales

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ import * as dotenv from 'dotenv';
 import express from 'express';
 import connectDB from './config/database';
 import userRoutes from './routes/user.routes';
+import branchRoutes from './routes/branch.routes';
 
 dotenv.config();
 
@@ -15,6 +16,7 @@ app.use(express.json());
 
 // Rutas
 app.use('/api/users', userRoutes);
+app.use('/api/branches', branchRoutes);
 
 app.listen(port, () => {
   console.log(`Server running on port ${port}`);
